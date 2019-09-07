0cc5d93355d4a6953b4d144586ba48ed78ffec38
Max Dauber <max@dauberfamily.com>
2019-09-06 23:44:42 -0400

    setting up running environment

---
+++ Makefile
@@ -0,0 +1,2 @@
+yash: yash.o
+	gcc -o yash yash.c -lreadline
--- yash.c
+++ yash.c
@@ -4,23 +4,20 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
-char *prompt = "> ";
-
 int main(){
+    char *prompt = "> ";
     int cpid, status;
     char *cmd;
 
-    while(cmd =readline(prompt)){
+    while(cmd = readline(prompt)){
         cpid = fork();
         if(cpid == 0){
             //child
-            printf("child cpid: %d",cpid);
             execlp(cmd,cmd,(char*)NULL);
         }
         else{
             //parent
-            printf("parent cpid: %d",cpid);
-            wait(&status);
+            //wait(&status);
         }
     }
 }
