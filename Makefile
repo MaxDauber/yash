f0d341e9150104b960602f38257cd12022eb64b6
Max Dauber <max@dauberfamily.com>
2019-09-13 01:40:42 -0400

    initial build

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
-yash: yash.o
-	gcc -o yash yash.c -lreadline
+# EE461S Project 1 Yash
+# Max Dauber mjd3375
+
+yash: yash.c
+	gcc -o yash yash.c -lreadline -I.
--- yash.c
+++ yash.c
@@ -1,23 +1,48 @@
+/**
+ * EE461S - Project 1
+ * Yash Shell
+ * Max Dauber (mjd3375)
+ */
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <string.h>
+#include <wait.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 
-int main(){
-    char *prompt = "> ";
-    int cpid, status;
-    char *cmd;
+char *prompt = "> ";
 
-    while(cmd = readline(prompt)){
-        cpid = fork();
-        if(cpid == 0){
-            //child
-            execlp(cmd,cmd,(char*)NULL);
-        }
-        else{
-            //parent
-            //wait(&status);
-        }
+void process(char* cmd){
+    int status;
+    int cpid = fork();
+
+    // split cmd into array of strings
+    char * cmd_array [100];
+    int index = 0;
+    cmd_array[index] = strtok(cmd," ");
+
+    while(cmd_array[index] != NULL){
+        cmd_array[++index] = strtok(NULL," ");
+    }
+    cmd_array[++index] = NULL;
+
+    if(cpid == 0){
+        execvp(cmd_array[0], cmd_array);
+    }
+    else{
+        wait(&status);
+    }
+}
+
+int main(){
+    //initialize_env();
+
+    while(1) {
+        char* cmd = readline(prompt);
+        if (!cmd) continue;
+        process(cmd);
+        //update jobs
     }
 }
