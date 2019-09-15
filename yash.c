a45a947d765058572e3d168da09ecbe0078f2052
Max Dauber <max@dauberfamily.com>
2019-09-15 06:41:37 -0400

    piping

--- yash.c
+++ yash.c
@@ -116,18 +116,69 @@ void execute_cmd(char*cmd[], int arg_count, int bg){
     }
 }
 
+void execute_pipe(char*cmd1[], int arg_count1, char*cmd2[], 
+    int arg_count2,int bg){
+    int pid = fork();
+    if (pid < 0){
+        perror("fork failed");
+        // exit(1);
+    }else if(pid == 0){
+        set_operators(cmd1, arg_count1);
+        execvp(cmd1[0], cmd1);
+    }
+    else{
+        if(!bg){
+            wait(NULL);
+        }
+    }
+}
+
 void process(char* cmd){
     char * args [MAX_ARGS];
     int arg_count = 0;
+    int pipe_address = -1;
     args[arg_count] = strtok(cmd," ");
 
     while(args[arg_count] != NULL){
+        
+        //append next token to command array
         args[++arg_count] = strtok(NULL," ");
 
+        //check if command is a pipe
+        if(args[arg_count] != NULL && 
+            strcmp(args[arg_count], PIPE) == 0){
+            pipe_address = arg_count;
+            printf("pipe is at index %d",pipe_address);
+        }
     }
+
     args[++arg_count] = NULL;
 
-    execute_cmd(args, arg_count, 0); 
+    if(pipe_address > 0){
+
+        // populate cmd1 with first command before pipe
+        char * cmd1 [pipe_address+1];
+        for(int i = 0; i < pipe_address; i++){
+            cmd1[i] = args[i];
+        }
+        cmd1[pipe_address] = NULL;
+
+        // populate cmd2 with command after pipe
+        char * cmd2 [arg_count - pipe_address + 2];
+        for(int i = 0; i < arg_count - pipe_address + 1; i++){
+            cmd2[i] = args[i];
+        }
+        cmd2[arg_count - pipe_address + 1] = NULL;
+        
+
+        //execute pipe command
+        execute_pipe(cmd1,pipe_address, cmd2, 
+            arg_count - pipe_address + 1, 0);
+    }
+    else{
+        //if no pipe execute regular command
+        execute_cmd(args, arg_count, 0);
+    } 
 }
 
 int main(){
